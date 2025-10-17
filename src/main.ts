c43f4ca124c79fd0a7201c0ac615360c6771aaf6
mlau16 <mlau16@ucsc.edu>
2025-10-17 00:19:35 +0000

    - Added canvas and canvas properties - Added Title (Step 1 done)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+self.addEventListener("DOMContentLoaded", () => {
+  setupUI();
+});
+
+function setupUI(): void {
+  const title: HTMLHeadingElement = document.createElement("h1");
+  title.textContent = "Sticker Sketchpad";
+  document.body.appendChild(title);
+
+  const canvas: HTMLCanvasElement = document.createElement("canvas");
+  canvas.id = "app-canvas";
+  canvas.width = 256;
+  canvas.height = 256;
+  document.body.appendChild(canvas);
+}
