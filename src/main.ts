fffbd72772708c3c4f0457224b232417e8999622
mlau16 <mlau16@ucsc.edu>
2025-10-17 02:27:34 +0000

    - Addad array - Added drawing-changed event
    Co-authored-by: dfang10 <dfang10@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -15,13 +15,13 @@ function setupUI(): void {
   canvas.height = 256;
   document.body.appendChild(canvas);
 
-  //Clear Button
-  const clearButton: HTMLButtonElement = document.createElement("button");
-  clearButton.textContent = "Clear";
-  document.body.appendChild(clearButton);
-
   const ctx = canvas.getContext("2d");
   if (!ctx) return;
+
+  const lines: { x: number; y: number }[][] = [];
+  const redoLines: { x: number; y: number }[][] = [];
+
+  let currentLine: { x: number; y: number }[] | null;
 
   ctx.lineWidth = 2;
   ctx.strokeStyle = "black";
@@ -33,28 +33,62 @@ function setupUI(): void {
     cursor.active = true;
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+
+    currentLine = [];
+    lines.push(currentLine);
+    redoLines.splice(0, redoLines.length);
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
   });
 
   canvas.addEventListener("mousemove", (e) => {
+    if (!currentLine) {
+      throw Error("");
+    }
     if (cursor.active) {
-      ctx.beginPath();
-      ctx.moveTo(cursor.x, cursor.y);
-      ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.stroke();
+      //ctx.beginPath();
+      //ctx.moveTo(cursor.x, cursor.y);
+      //ctx.lineTo(e.offsetX, e.offsetY);
+      //ctx.stroke();
       cursor.x = e.offsetX;
       cursor.y = e.offsetY;
     }
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
   });
 
   canvas.addEventListener("mouseup", () => {
     cursor.active = false;
+    currentLine = null;
+
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
   });
 
-  canvas.addEventListener("mouseleave", () => {
-    cursor.active = false;
-  });
+  // canvas.addEventListener("mouseleave", () => {
+  //   cursor.active = false;
 
-  clearButton.addEventListener("click", () => {
+  //   canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+  // });
+
+  function redraw() {
+    if (!ctx) {
+      throw Error("Error! Unsupported browser.");
+    }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  });
+    for (const line of lines) {
+      if (line.length > 1) {
+        ctx.beginPath();
+        const { x, y } = line[0];
+        ctx.moveTo(x, y);
+        for (const { x, y } of line) {
+          ctx.lineTo(x, y);
+        }
+        ctx.stroke();
+      }
+    }
+  }
+
+  canvas.addEventListener("drawing-changed", redraw);
 }
