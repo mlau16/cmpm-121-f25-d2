b9e9480e04be8b508b211a58ffec4bc3fd821321
mlau16 <mlau16@ucsc.edu>
2025-10-17 00:30:29 +0000

    - Added drawing - Added clear button (Step 2 done)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-
 import "./style.css";
 
 self.addEventListener("DOMContentLoaded", () => {
@@ -15,4 +14,47 @@ function setupUI(): void {
   canvas.width = 256;
   canvas.height = 256;
   document.body.appendChild(canvas);
+
+  //Clear Button
+  const clearButton: HTMLButtonElement = document.createElement("button");
+  clearButton.textContent = "Clear";
+  document.body.appendChild(clearButton);
+
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = "black";
+  ctx.lineCap = "round";
+
+  const cursor = { active: false, x: 0, y: 0 };
+
+  canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+      ctx.beginPath();
+      ctx.moveTo(cursor.x, cursor.y);
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      cursor.x = e.offsetX;
+      cursor.y = e.offsetY;
+    }
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    cursor.active = false;
+  });
+
+  canvas.addEventListener("mouseleave", () => {
+    cursor.active = false;
+  });
+
+  clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
 }
